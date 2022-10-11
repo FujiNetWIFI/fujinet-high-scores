1f0933f104db3b3086d6cf9d3d97c8af6364ca74
Thomas <thom.cherryhomes@gmail.com>
2022-10-11 17:40:37 -0500

    change title.

--- atari/jawbreakerii/jawbreakerii.c
+++ atari/jawbreakerii/jawbreakerii.c
@@ -63,7 +63,7 @@ void jawbreakerii(char *atr, char *html)
   fprintf(fh,"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n");
   fprintf(fh,"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n");
   fprintf(fh," <head>\n");
-  fprintf(fh,"  <title>Latest Frogger High Scores</title>\n");
+  fprintf(fh,"  <title>Latest Jawbreaker II High Scores</title>\n");
   fprintf(fh,"  <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n");
   fprintf(fh,"  <meta http-equiv=\"refresh\" content=\"30\" />");
   fprintf(fh,"  <meta name=\"keywords\" content=\" \" />\n");
