1d7e02880a8e276141b48fd11beeed0312915da3
Thomas <thom.cherryhomes@gmail.com>
2022-10-18 18:50:14 -0500

    add championship loderunner.

---
+++ atari/championshiploderunner/championshiploderunner.c
@@ -0,0 +1,179 @@
+/**
+ * Grab high score from Tutti Frutti, write to HTML
+ *
+ * Linux required. (uses inotify)
+ * 
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+
+#define EVENT_SIZE ( sizeof(struct inotify_event) )
+#define EVENT_BUF_LEN ( 1024 * ( EVENT_SIZE + NAME_MAX ) )
+
+#define LODERUNNER_OFFSET 0x14F90
+
+static volatile bool ctrlc = false;
+
+int inotify_fd, inotify_wd;
+int inotify_event_len;
+
+char event_buffer[EVENT_BUF_LEN];
+
+void setctrlc(int dummy)
+{
+  ctrlc = true;
+}
+
+char championshiploderunner_initial(unsigned char c)
+{
+  if (c==0)
+    return 0x20;
+  else
+    return c & 0x7F;
+}
+
+void championshiploderunner(char *atr, char *html)
+{
+  FILE *fa, *fh;
+  int i;
+  unsigned short len;
+
+  unsigned char buf[128];
+  char *p;
+
+  struct
+  {
+    unsigned char initials[3];
+    unsigned char level;
+    unsigned char score[4];
+  } s[10];
+  
+  printf("Writing new championshiploderunner.html\n");
+  
+  fa = fopen(atr,"rb");
+
+  fh = fopen(html,"w");
+  
+  /* start html */
+  fprintf(fh,"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n");
+  fprintf(fh,"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n");
+  fprintf(fh," <head>\n");
+  fprintf(fh,"  <title>Latest Championship Lode Runner High Scores</title>\n");
+  fprintf(fh,"  <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n");
+  fprintf(fh,"  <meta http-equiv=\"refresh\" content=\"30\" />");
+  fprintf(fh,"  <meta name=\"keywords\" content=\" \" />\n");
+  fprintf(fh,"  <meta name=\"description\" content=\" \" />\n");
+  fprintf(fh,"  <link rel=\"stylesheet\" type=\"text/css\" href=\"championshiploderunner.css\" media=\"screen\" />\n");
+  fprintf(fh,"  <link rel=\"icon\" type=\"image/png\" href=\"icon.png\" />\n");
+  fprintf(fh," </head>\n");
+  fprintf(fh," <body>\n");
+  fprintf(fh,"  <pre>\n");
+
+  /* start body */
+
+  fprintf(fh,"\n==== TOP SCORES ====\n\n");
+
+  p=strtok(buf,"\x9b");
+
+  fseek(fa,LODERUNNER_OFFSET,SEEK_SET);
+  fread(&s[0],sizeof(char),80,fa);
+
+  for (i=0;i<10;i++)
+    fprintf(fh,"%2u. %c%c%c %02u %02x%02x%02x%02x\n",i+1,
+	   championshiploderunner_initial(s[i].initials[0]),
+	   championshiploderunner_initial(s[i].initials[1]),
+	   championshiploderunner_initial(s[i].initials[2]),
+	   s[i].level,
+	   s[i].score[0],s[i].score[1],s[i].score[2],s[i].score[3]);
+  
+  /* end body */
+
+  fprintf(fh,"\n");
+  fprintf(fh,"  </pre>\n");
+  fprintf(fh," </body>\n");
+  fprintf(fh,"</html>\n");
+  
+  fclose(fh);
+  fclose(fa);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+    {
+      printf("%s <path-to-championshiploderunner-atr> <path-to-output-html>\n",argv[0]);
+      return 1;
+    }
+
+  championshiploderunner(argv[1],argv[2]);
+  
+  signal(SIGINT, setctrlc);
+  signal(SIGTERM, setctrlc);
+  
+  inotify_fd = inotify_init();
+
+  if (inotify_fd < 0)
+    {
+      perror("inotify_init");
+      return 1;
+    }
+
+  inotify_wd = inotify_add_watch(inotify_fd, argv[1], IN_MODIFY);
+
+  if (inotify_wd == -1)
+    {
+      perror("inotify_add_watch");
+      goto bye2;
+    }
+
+  /* Set for non-blocking */
+  fcntl (inotify_fd, F_SETFL, fcntl (inotify_fd, F_GETFL) | O_NONBLOCK);
+  
+  while (!ctrlc)
+    {
+      int i;
+      
+      inotify_event_len = read(inotify_fd, event_buffer, EVENT_BUF_LEN);
+
+      i=0;
+
+      if (inotify_event_len < 0)
+	{
+	  usleep(100000);
+	  continue;
+	}
+      
+      while (i < inotify_event_len)
+	{
+	  struct inotify_event *event = ( struct inotify_event * ) &event_buffer[ i ];
+
+	  championshiploderunner(argv[1],argv[2]);
+
+	  i += EVENT_SIZE + event->len;
+	}
+    }
+
+  /* ctrl-C or termination, close it off. */
+
+  printf("Exiting %s\n",argv[0]);
+ bye:
+  inotify_rm_watch(inotify_fd,inotify_wd);
+ bye2:
+  close(inotify_fd);
+
+  return 0;
+}
